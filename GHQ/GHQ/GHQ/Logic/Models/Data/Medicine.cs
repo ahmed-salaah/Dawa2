48cd165df27ea0174ba40fb917886a9f9c684ce4
mosta <mosta@DESKTOP-DC5002U>
2016-12-24 23:04:01 +0200

    aa

---
+++ GHQ/GHQ/GHQ/Logic/Models/Data/Medicine.cs
@@ -0,0 +1,41 @@
+﻿using Models;
+using System.Collections.Generic;
+
+namespace Logic.Models.Data
+{
+    public class Medicine : BaseModel
+    {
+        private string _UnifiedNumber;
+        public string UnifiedNumber
+        {
+            get
+            {
+                return _UnifiedNumber;
+            }
+            set
+            {
+                Set(() => UnifiedNumber, ref _UnifiedNumber, value);
+            }
+        }
+
+        private string _EmiratesID;
+        public string EmiratesID
+        {
+            get
+            {
+                return _EmiratesID;
+            }
+            set
+            {
+                Set(() => EmiratesID, ref _EmiratesID, value);
+            }
+        }
+
+        public override IEnumerable<ValidatedModel> Validate()
+        {
+            List<ValidatedModel> errors = new List<ValidatedModel>();
+           
+            return errors;
+        }
+    }
+}
---
+++ GHQ/GHQ/GHQ/UI/Pages/Medicine/MedicineHistory.xaml.cs
@@ -0,0 +1,21 @@
+﻿using GHQ.Logic;
+using GHQ.Logic.ViewModels.Account;
+
+namespace GHQ.UI.Pages.Medicine
+{
+    public partial class MedicineHistory : BasePage
+    {
+        //readonly NewAccountViewModel AccountVM = Locator.Default.NewAccountViewModel;
+        public MedicineHistory()
+        {
+            InitializeComponent();
+            //BindingContext = AccountVM;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            //AccountVM.OnIntializeCommand.Execute(null);
+        }
+    }
+}
