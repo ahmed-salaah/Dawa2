7f67b686ccaa655db8073fe6b774f0d556381d7c
Ahmed Salah <ahmed.salaheldin1790@gmail.com>
2017-01-18 02:08:41 +0200

    Notification service

---
+++ GHQ/ProjectX.Common/Common/Service/LocalNotifications/ILocalNotifications.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using System.Resources;
+
+namespace Service.ILocalNotifications
+{
+   
+    
+    public interface ILocalNotifications
+    {
+		void showNotification(string title, string body, DateTime date, string sound);
+    }
+
+   
+}
