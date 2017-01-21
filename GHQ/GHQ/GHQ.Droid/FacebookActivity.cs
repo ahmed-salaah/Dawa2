194eab5457121f05a4fd1d87486c6eb842058c7b
mosta <mosta@DESKTOP-DC5002U>
2017-01-21 18:34:00 +0200

    Adding FB android SDK

--- GHQ/GHQ/GHQ.Droid/FacebookActivity.cs
+++ GHQ/GHQ/GHQ.Droid/FacebookActivity.cs
@@ -11,35 +11,35 @@
 //    [Activity(Label = "")]
 //    public class FacebookActivity : Activity, Session.IStatusCallback
 //    {
-//            protected override void OnCreate(Bundle bundle)
-//            {
-//                base.OnCreate(bundle);
+//        protected override void OnCreate(Bundle bundle)
+//        {
+//            base.OnCreate(bundle);
 
 
-//                // Open a FB Session and show login if necessary
-//                Session.OpenActiveSession(this, true, this);
-//            }
-
-//            protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
-//            {
-//                base.OnActivityResult(requestCode, resultCode, data);
-
-//                // Relay the result to our FB Session
-//                Session.ActiveSession.OnActivityResult(this, requestCode, (int)resultCode, data);
-//            }
-
-//            public void Call(Session session, SessionState state, Exception exception)
-//            {
-//                // Make a request for 'Me' information about the current user
-//                if (session.IsOpened)
-//                {
-//                    Intent mainIntent = new Intent(this, typeof(MainActivity));
-//                    mainIntent.PutExtra("facebookToken", session.AccessToken);
-//                    SetResult(Result.FirstUser, mainIntent);
-//                    Finish();
-//                }
-
-//            }
+//            // Open a FB Session and show login if necessary
+//            Session.OpenActiveSession(this, true, this);
 //        }
-    
+
+//        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+//        {
+//            base.OnActivityResult(requestCode, resultCode, data);
+
+//            // Relay the result to our FB Session
+//            Session.ActiveSession.OnActivityResult(this, requestCode, (int)resultCode, data);
+//        }
+
+//        public void Call(Session session, SessionState state, Exception exception)
+//        {
+//            // Make a request for 'Me' information about the current user
+//            if (session.IsOpened)
+//            {
+//                Intent mainIntent = new Intent(this, typeof(MainActivity));
+//                mainIntent.PutExtra("facebookToken", session.AccessToken);
+//                SetResult(Result.FirstUser, mainIntent);
+//                Finish();
+//            }
+
+//        }
+//    }
+
 //}
