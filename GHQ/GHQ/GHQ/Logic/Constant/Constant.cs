841a805998a4b32893095c09f7912024eb8059aa
mosta <mosta@DESKTOP-DC5002U>
2017-01-22 10:13:46 +0200

    Build

--- GHQ/GHQ/GHQ/Logic/Constant/Constant.cs
+++ GHQ/GHQ/GHQ/Logic/Constant/Constant.cs
@@ -3,11 +3,7 @@
     public class Constant
     {
         public const bool IsProduction = false;
-        public const string DefaultWorkingTypeId = "Working";
-        public const string DefaultMaritalStatusId = "3";
-        public const bool UseServiceRegisterationAsWebView = false;
         public const string ResourceId = "GHQ.Resources.Strings.AppResources";
-        public const int MaxSendingCode = 10;
 		public const string UserIDKey = "ID";
 
     }
